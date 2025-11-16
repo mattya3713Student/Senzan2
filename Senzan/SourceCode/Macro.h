f75fb99db0b3a81bc38cf57bd5d74d38fec84652
mattya3713 <mattya3713gpsibou@gmai.com>
2025-11-16 10:51:10 +0900

    enumマクロ定義

--- Senzan/SourceCode/Macro.h
+++ Senzan/SourceCode/Macro.h
@@ -8,6 +8,42 @@
 // 配列破棄.
 #define SAFE_DELETE_ARRAY(p)do{if(p!=nullptr){delete[](p);(p) = nullptr;}}while(0)
 
+// enumにビット演算を定義.
+
+// トレイト構造体: EnumType がビットフラグとして有効化されているかを示すマーカー.
+template <typename T>
+struct is_scoped_enum_flag : std::false_type {};
+
+// 基底型の別名定義.
+template <typename T>
+using underlying_type = std::underlying_type_t<T>;
+
+// テンプレート関数を有効化するためのエイリアス.
+template <typename T>
+using enable_if_flag = std::enable_if_t<is_scoped_enum_flag<T>::value, T>;
+template <typename T>
+using enable_if_flag_ref = std::enable_if_t<is_scoped_enum_flag<T>::value, T&>;
+template <typename T>
+struct is_scoped_enum_flag : std::false_type {};
+
+// | 演算子 (OR).
+template <typename T> enable_if_flag<T> operator|(T a, T b) noexcept { return static_cast<T>(static_cast<underlying_type<T>>(a) | static_cast<underlying_type<T>>(b)); }
+// & 演算子 (AND).
+template <typename T> enable_if_flag<T> operator&(T a, T b) noexcept { return static_cast<T>(static_cast<underlying_type<T>>(a) & static_cast<underlying_type<T>>(b)); }
+// ^ 演算子 (XOR).
+template <typename T> enable_if_flag<T> operator^(T a, T b) noexcept { return static_cast<T>(static_cast<underlying_type<T>>(a) ^ static_cast<underlying_type<T>>(b)); }
+// ~ 演算子 (NOT/反転).
+template <typename T> enable_if_flag<T> operator~(T a) noexcept { return static_cast<T>(~static_cast<underlying_type<T>>(a)); }
+// |= 演算子 (OR 代入).
+template <typename T> enable_if_flag_ref<T> operator|=(T& a, T b) noexcept { a = a | b; return a; } 
+// &= 演算子 (AND 代入).
+template <typename T> enable_if_flag_ref<T> operator&=(T& a, T b) noexcept { a = a & b; return a; }
+// ^= 演算子 (XOR 代入).
+template <typename T> enable_if_flag_ref<T> operator^=(T& a, T b) noexcept { a = a ^ b; return a; }
+
+#define ENABLE_ENUM_FLAG_OPERATORS(EnumType) \
+    template <> struct is_scoped_enum_flag<EnumType> : std::true_type {};
+
 // D3DXMATRIXをDirectX::XMMATRIXに変換.
 inline DirectX::XMMATRIX D3DXMatrixToXMMatrix(const D3DXMATRIX & d3d9Matrix)
 {
